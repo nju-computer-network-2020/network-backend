618d2c67cf2b9abd30f56611ee343e85c4b14168
Junda Liao <284983325@qq.com>
2020-12-20 21:51:21 +0800

    Add commands for RTB and RTC in typology.py

--- flaskr/topology.py
+++ flaskr/topology.py
@@ -1,15 +1,19 @@
+# Some topology configuration information and commands
+
+# hostnames
 RTA = 'RTA'
 RTB = 'RTB'
 RTC = 'RTC'
 
 valid_hosts = {RTA, RTB, RTC}
 
+# commands to be executed on RTA
 _commands_rta = [
     'configure terminal',
     'interface f0/0',
     'ip address 192.168.3.1 255.255.255.0',
     'no shutdown',
-    'interface s2/0',
+    'interface s0/0/1',
     'ip address 192.168.1.1 255.255.255.252',
     'no shutdown',
     'clock rate 9600',
@@ -19,10 +23,40 @@ _commands_rta = [
     'exit'
 ]
 
-_commands_rtb = []
+# commands to be executed on RTB
+_commands_rtb = [
+    "configure terminal",
+    "interface f0/0",
+    "ip address 10.0.0.1 255.0.0.0",
+    "no shutdown",
+    "interface s0/0/1",
+    "ip address 192.168.1.2 255.255.255.252",
+    "no shutdown",
+    "clock rate 9600",
+    'exit',
+    'ip route 0.0.0.0 0.0.0.0 192.168.1.1',
+    'ip nat inside source static 10.0.0.2 192.168.1.34',
+    'ip nat inside source static 10.0.0.11 192.168.1.35',
+    'interface f0/0',
+    'ip nat inside',
+    'interface s0/0/1',
+    'ip nat outside',
+    'exit',
+    'exit',
+    'exit'
+]
 
-_commands_rtc = []
-
+# commands to be executed on RTC
+_commands_rtc = [
+    'configure terminal',
+    'interface f0/0',
+    'ip address 10.0.0.2 255.0.0.0',
+    'no shutdown',
+    'exit',
+    'ip route 0.0.0.0 0.0.0.0 10.0.0.1',
+    'exit',
+    'exit'
+]
 
 commands = {
     RTA: _commands_rta,
